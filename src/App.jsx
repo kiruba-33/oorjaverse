dbce9c1706022ef216475c398071d3f17c3d1beb
kiruba-33 <kirubakaran1890@gmail.com>
2025-11-18 15:20:09 +0530

    fix the app.jsx routes issues

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,6 @@ function App() {
 
       <Suspense fallback={<Preloader />}>
       <Navbar/>
-      <AnimatedRoutes/>
         <MouseFollower />
         <ScrollToTopButton/>
         <FloatingWhatsapp/>
