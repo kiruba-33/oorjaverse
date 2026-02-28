6f3ba94b8aebee1e28fca02d7c036f30a02b4231
kiruba-33 <kirubakaran1890@gmail.com>
2026-02-28 22:11:52 +0530

    number added

--- src/components/FloatingWhatsapp.jsx
+++ src/components/FloatingWhatsapp.jsx
@@ -1,7 +1,7 @@
 import { FaWhatsapp } from "react-icons/fa";
 
 export default function FloatingWhatsapp() {
-  const whatsappNumber = "+91 8608865303"; // example: 919876543210
+  const whatsappNumber = "YOUR_PHONE_NUMBER"; // example: 919876543210
 
   return (
      <a
